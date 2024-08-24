d8ee9c44b5a52fce0ecebeaa58fb193d30486fbf
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-08-24 21:12:52 +0100

    Renamed TypeRegistrar to MyTypeRegistrar, making it more obvious it is a custom class the user must provide.

--- examples/Cli/Injection/Program.cs
+++ examples/Cli/Injection/Program.cs
@@ -14,7 +14,7 @@ public class Program
 
         // Create a type registrar and register any dependencies.
         // A type registrar is an adapter for a DI framework.
-        var registrar = new TypeRegistrar(registrations);
+        var registrar = new MyTypeRegistrar(registrations);
 
         // Create a new command app with the registrar
         // and run it with the provided arguments.

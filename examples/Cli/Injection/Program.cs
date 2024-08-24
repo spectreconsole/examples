3d98d3e637e43dc6cbac372021f7d0938b666005
Frank Ray <52075808+FrankRay78@users.noreply.github.com>
2024-08-24 21:02:02 +0100

    Update code example to align with snippet in CommandApp/DI documentation

--- examples/Cli/Injection/Program.cs
+++ examples/Cli/Injection/Program.cs
@@ -9,10 +9,11 @@ public class Program
 {
     public static int Main(string[] args)
     {
-        // Create a type registrar and register any dependencies.
-        // A type registrar is an adapter for a DI framework.
         var registrations = new ServiceCollection();
         registrations.AddSingleton<IGreeter, HelloWorldGreeter>();
+
+        // Create a type registrar and register any dependencies.
+        // A type registrar is an adapter for a DI framework.
         var registrar = new TypeRegistrar(registrations);
 
         // Create a new command app with the registrar

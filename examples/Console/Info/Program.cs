952bd4a6cdb65d8f48ec2b078a3a12ba684593ab
Patrik Svensson <patrik@patriksvensson.se>
2026-04-17 00:52:36 +0200

    Update dependencies

--- examples/Console/Info/Program.cs
+++ examples/Console/Info/Program.cs
@@ -14,7 +14,6 @@ public static class Program
             .AddRow("[b]Unicode?[/]", $"{YesNo(AnsiConsole.Profile.Capabilities.Unicode)}")
             .AddRow("[b]Supports ansi?[/]", $"{YesNo(AnsiConsole.Profile.Capabilities.Ansi)}")
             .AddRow("[b]Supports links?[/]", $"{YesNo(AnsiConsole.Profile.Capabilities.Links)}")
-            .AddRow("[b]Legacy console?[/]", $"{YesNo(AnsiConsole.Profile.Capabilities.Legacy)}")
             .AddRow("[b]Interactive?[/]", $"{YesNo(AnsiConsole.Profile.Capabilities.Interactive)}")
             .AddRow("[b]Terminal?[/]", $"{YesNo(AnsiConsole.Profile.Out.IsTerminal)}")
             .AddRow("[b]Buffer width[/]", $"{AnsiConsole.Console.Profile.Width}")
